e8dbb58935310a5de722a591830feb668a10b6f1
Matthias Viehweger <kronn@kronn.de>
2010-09-01 18:45:31 +0200

    put basic version in place

--- lib/rack-speling/version.rb
+++ lib/rack-speling/version.rb
@@ -1,5 +1,15 @@
-module Rack
+# vim:ft=ruby:fileencoding=utf-8
+
+module Rack # :nodoc:
   module Speling
     VERSION = "0.0.1"
+
+    SUMMARY = "Simple Speling corretor in Rack"
+
+    DESCRIPTION = <<-EOD
+      Rack::Speling is a simple port of mod_speling to Rack.
+
+      Currently, only case-insensitivety is achieved.
+    EOD
   end
 end
